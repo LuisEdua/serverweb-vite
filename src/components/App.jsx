3b3cc44b6206a15a217df8b3553f9842f8f81c00
Luis Eduardo <100817774+LuisEdua@users.noreply.github.com>
2022-09-27 23:13:27 -0500

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
+import '../assets/styles/App.css'
 import Canciones from "./Canciones"
 import Layout from "./Layout"
 function App() {
   return (
     <>
       <Layout>
-        <Canciones></Canciones>
+        <Canciones/>
       </Layout>
     </>
   )
